79a6637ad26a7216fa55bbdf309cb98f9bc4437d
ANISH <153320703+codex-ML@users.noreply.github.com>
2024-08-12 10:20:50 +0530

    Update Youtube.py

--- EsproAiMusic/platforms/Youtube.py
+++ EsproAiMusic/platforms/Youtube.py
@@ -249,7 +249,7 @@ class YouTubeAPI:
                 "nocheckcertificate": True,
                 "quiet": True,
                 "proxy": "socks5://127.0.0.1:9050",
-                # "cookies": cookies_file,
+                "cookiefile": cookies_file,
                 "no_warnings": True,
             }
             x = yt_dlp.YoutubeDL(ydl_optssx)
@@ -268,7 +268,7 @@ class YouTubeAPI:
                 "nocheckcertificate": True,
                 "quiet": True,
                 "proxy": "socks5://127.0.0.1:9050",
-                # "cookies": cookies_file,
+                "cookiefile": cookies_file,
                 "no_warnings": True,
             }
             x = yt_dlp.YoutubeDL(ydl_optssx)
@@ -291,7 +291,7 @@ class YouTubeAPI:
                 "no_warnings": True,
                 "prefer_ffmpeg": True,
                 "proxy": "socks5://127.0.0.1:9050",
-                # "cookies": cookies_file,
+                "cookiefile": cookies_file,
                 "merge_output_format": "mp4",
             }
             x = yt_dlp.YoutubeDL(ydl_optssx)
@@ -306,7 +306,7 @@ class YouTubeAPI:
                 "nocheckcertificate": True,
                 "quiet": True,
                 "proxy": "socks5://127.0.0.1:9050",
-                # "cookies": cookies_file,
+                "cookiefile": cookies_file,
                 "no_warnings": True,
                 "prefer_ffmpeg": True,
                 "postprocessors": [
